b76badc63d2e075958ab6359b7e05abc1074ed8e
zz-lin-tech <zz.lin1886@gmail.com>
2024-07-29 17:34:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ import pdfplumber
 import pandas as pd
 import os
 import tempfile
-import time
 
 def process_pdfs(pdf_files):
     all_data = []
+
     for pdf_path in pdf_files:
         with pdfplumber.open(pdf_path) as pdf:
             data = []
@@ -18,9 +18,11 @@ def process_pdfs(pdf_files):
                         for row in table:
                             cleaned_row = [cell.replace('\n', '') if isinstance(cell, str) else cell for cell in row]
                             data.append(cleaned_row)
+
             df = pd.DataFrame(data)
             start_indices = df[df.apply(lambda row: row.astype(str).str.contains('表体').any(), axis=1)].index
             end_indices = df[df.apply(lambda row: row.astype(str).str.contains('报关单草稿').any(), axis=1)].index
+
             if len(start_indices) > 0 and len(end_indices) > 0:
                 start_index = start_indices[0] + 2
                 end_index = end_indices[0]
@@ -34,6 +36,7 @@ def process_pdfs(pdf_files):
                 df_filtered["申报数量"] = pd.to_numeric(df_filtered["申报数量"], errors='coerce')
                 df_filtered["申报总价"] = pd.to_numeric(df_filtered["申报总价"], errors='coerce')
                 all_data.append(df_filtered)
+
     combined_df = pd.concat(all_data, ignore_index=True)
     combined_df["累计申报数量"] = combined_df.groupby("备案序号")["申报数量"].transform('sum')
     combined_df["累计申报总价"] = combined_df.groupby("备案序号")["申报总价"].transform('sum')
@@ -46,19 +49,8 @@ def process_pdfs(pdf_files):
     return combined_df
 
 st.title('PDF to Excel Processor')
-st.markdown("""
-### 使用说明
-1. 点击 **Browse files** 按钮上传一个或多个 PDF 文件。
-2. 上传完成后，点击 **Process** 按钮处理文件。
-3. 处理完成后，点击 **Download Excel file** 按钮下载生成的 Excel 文件。
-""")
 
 uploaded_files = st.file_uploader("Upload PDF files", type="pdf", accept_multiple_files=True)
-
-def keep_alive():
-    while True:
-        time.sleep(60 * 5)  # 每5分钟发送一次请求
-        st.write("Keep alive")
 
 if st.button('Process'):
     if uploaded_files:
@@ -67,17 +59,16 @@ if st.button('Process'):
             with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as tmp_file:
                 tmp_file.write(uploaded_file.getbuffer())
                 file_paths.append(tmp_file.name)
+
         result_df = process_pdfs(file_paths)
         output_path = os.path.join(tempfile.gettempdir(), 'A客户.xlsx')
         result_df.to_excel(output_path, index=False)
+
         with open(output_path, "rb") as f:
             st.download_button(label="Download Excel file", data=f, file_name="A客户.xlsx",
                                mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
     else:
         st.error("Please upload at least one PDF file.")
-else:
-    keep_alive()  # 开启心跳机制
-
 
 
 
